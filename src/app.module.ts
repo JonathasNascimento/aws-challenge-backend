9c67089d82a0f8ba6bf58c82d34a6e8203ced737
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-19 12:39:07 -0300

    added device service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DeviceController } from './controllers/device/device.controller';
+import { DeviceService } from './services/device/device.service';
 
 @Module({
   imports: [],
   controllers: [AppController, DeviceController],
-  providers: [AppService],
+  providers: [AppService, DeviceService],
 })
 export class AppModule {}
