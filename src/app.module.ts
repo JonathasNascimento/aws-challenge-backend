2289566fbe77c379c051d59cb6464ec32031707a
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-19 19:13:04 -0300

    added prisma client to connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DeviceController } from './controllers/device/device.controller';
 import { DeviceService } from './services/device/device.service';
+import { PrismaService } from './services/prisma/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController, DeviceController],
-  providers: [AppService, DeviceService],
+  providers: [AppService, DeviceService, PrismaService],
 })
 export class AppModule {}
