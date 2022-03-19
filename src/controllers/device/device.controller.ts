f7f22815f948419b184b9ed26ba2a5043d54ea6b
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-19 16:15:09 -0300

    added findDevices and findDeviceById in the class DeviceController

--- src/controllers/device/device.controller.ts
+++ src/controllers/device/device.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Device } from 'src/models/device';
+import { DeviceService } from 'src/services/device/device.service';
 
 @Controller('device')
-export class DeviceController {}
+export class DeviceController {
+  constructor(private readonly deviceService: DeviceService) { }
+
+  @Get()
+  findDevices(): [Device] {
+    return this.deviceService.findDevices();
+  }
+
+  @Get(':id')
+  findDeviceById(@Param() params): Device {
+    return this.deviceService.findDeviceById(params.id);
+  }
+}
