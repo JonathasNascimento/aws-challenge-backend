e92eea2c5671042775386e1b518117ab989de018
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-19 16:03:30 -0300

    created findDevices and findDeviceById with mock

--- src/services/device/device.service.ts
+++ src/services/device/device.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Category } from 'src/models/category';
+import { Device } from 'src/models/device';
 
 @Injectable()
-export class DeviceService {}
+export class DeviceService {
+  findDevices(): [Device] {
+    const device = new Device();
+    device.id = 1;
+    device.category = new Category();
+    device.color = 'black';
+    device.partNumber = 1;
+
+    return [device];
+  }
+
+  findDeviceById(deviceId: number): Device {
+    const device = new Device();
+    device.id = 1;
+    device.category = new Category();
+    device.color = 'black';
+    device.partNumber = 1;
+
+    return device;
+  }
+}
