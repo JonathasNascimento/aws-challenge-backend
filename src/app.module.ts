94b4fc1f1f14528f7f43fd5b04b8621175bf1053
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-19 22:13:06 -0300

    add DeviceService removed from app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DeviceController } from './controllers/device/device.controller';
 import { DeviceRepository } from './repositories/device/device.repository';
+import { DeviceService } from './services/device/device.service';
 import { PrismaService } from './services/prisma/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController, DeviceController],
-  providers: [AppService, DeviceRepository, PrismaService],
+  providers: [AppService, DeviceService, DeviceRepository, PrismaService],
 })
 export class AppModule { }
