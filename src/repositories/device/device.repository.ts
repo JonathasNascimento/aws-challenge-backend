1d80b8baacb34913fb61906418b806ec497e93d1
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-19 21:44:42 -0300

    added findAll and findById to DeviceRepository

--- src/repositories/device/device.repository.ts
+++ src/repositories/device/device.repository.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Device } from '@prisma/client';
 import { PrismaService } from 'src/services/prisma/prisma.service';
 
 @Injectable()
 export class DeviceRepository {
   constructor(private readonly prisma: PrismaService) { }
+
+  findAll(): Promise<Device[]> {
+    return this.prisma.device.findMany();
+  }
+
+  findById(id: number): Promise<Device> {
+    return this.prisma.device.findUnique({ where: { id } });
+  }
 }
