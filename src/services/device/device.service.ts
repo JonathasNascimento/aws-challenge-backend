ec61b05a5fa7fecbd73605a17d938c79efcc1e91
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-19 21:08:05 -0300

    integrate deviceService with prisma

--- src/services/device/device.service.ts
+++ src/services/device/device.service.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { Category } from 'src/models/category';
-import { Device } from 'src/models/device';
+import { Device } from '@prisma/client';
+
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class DeviceService {
-  findDevices(): [Device] {
-    const device = new Device();
-    device.id = 1;
-    device.category = new Category();
-    device.color = 'black';
-    device.partNumber = 1;
+  constructor(private readonly prisma: PrismaService) { }
 
-    return [device];
+  findDevices(): Promise<Device[]> {
+    return this.prisma.device.findMany();
   }
 
-  findDeviceById(deviceId: number): Device {
-    const device = new Device();
-    device.id = 1;
-    device.category = new Category();
-    device.color = 'black';
-    device.partNumber = 1;
-
-    return device;
+  findDeviceById(id: number): Promise<Device> {
+    return this.prisma.device.findUnique({ where: { id } });
   }
 }
