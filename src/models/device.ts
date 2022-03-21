e84f55c97bb824b5710ed2c609c81e115da2c6c1
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-21 20:53:01 -0300

    setup device validation

--- src/models/device.ts
+++ src/models/device.ts
@@ -1,6 +1,19 @@
-export interface Device {
+import { IsAlpha, IsNotEmpty, IsNumber, IsPositive, IsString, MaxLength } from 'class-validator';
+
+export class Device {
   id: number;
+
+  @IsNumber()
   categoryId: number;
+
+  @IsAlpha()
+  @IsString()
+  @MaxLength(16)
+  @IsNotEmpty()
   color: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   partNumber: number;
 }
