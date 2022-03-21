86ec9c6420d93b9fa7594f651d8340b42b62b838
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-21 20:10:35 -0300

    added tests for device controller

--- src/controllers/device/device.controller.spec.ts
+++ src/controllers/device/device.controller.spec.ts
@@ -7,6 +7,8 @@ import { DeviceController } from './device.controller';
 
 describe('DeviceController', () => {
   let controller: DeviceController;
+  let service: DeviceService;
+  let device;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,9 +17,47 @@ describe('DeviceController', () => {
     }).compile();
 
     controller = module.get<DeviceController>(DeviceController);
+    service = module.get<DeviceService>(DeviceService);
+
+    device = {
+      id: 1,
+      categoryId: 1,
+      color: 'black',
+      partNumber: 1,
+    };
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findDevices', () => {
+    it('should fetch all devices', async () => {
+      jest
+        .spyOn(service, 'findDevices')
+        .mockImplementation(() => Promise.resolve([device]));
+      expect(await controller.findDevices()).toStrictEqual([{ ...device }]);
+    });
+
+    it('should fetch one device', async () => {
+      jest.spyOn(service, 'findDeviceById').mockImplementation(() => device);
+      expect(await controller.findDeviceById({ params: { id: 1 } })).toBe(
+        device,
+      );
+    });
+  });
+
+  describe('createDevice', () => {
+    it('should create one device', async () => {
+      jest.spyOn(service, 'createNewDevice').mockImplementation(() => device);
+      expect(await controller.createDevice(device)).toBe(device);
+    });
+  });
+
+  describe('deleteDevice', () => {
+    it('should delete a device', async () => {
+      jest.spyOn(service, 'deleteDevice').mockImplementation(() => device);
+      expect(await controller.deleteDevice({ params: { id: 1 } })).toBe(device);
+    });
   });
 });
