f9b73dc27e541e01785fc54fbfd66b76687c2d7e
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-22 07:15:36 -0300

    added implementation to category repository

--- src/repositories/category/category.repository.ts
+++ src/repositories/category/category.repository.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Category as CategoryModel } from 'src/models/category';
+
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class CategoryRepository {}
+export class CategoryRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll(): Promise<CategoryModel[]> {
+    return Promise.resolve([]);
+  }
+
+  findById(): Promise<CategoryModel | null> {
+    return Promise.resolve(null);
+  }
+
+  createNew(): Promise<CategoryModel | null> {
+    return Promise.resolve(null);
+  }
+
+  delete(): Promise<CategoryModel | null> {
+    return Promise.resolve(null);
+  }
+}
