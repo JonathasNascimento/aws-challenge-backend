4d57aea81feaaf3f583080b2265ff4fe5ec27c6f
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-22 06:34:01 -0300

    added category feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,24 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoryController } from './controllers/category/category.controller';
 import { DeviceController } from './controllers/device/device.controller';
+import { CategoryRepository } from './repositories/category/category.repository';
 import { DeviceRepository } from './repositories/device/device.repository';
 import { PrismaService } from './repositories/prisma/prisma.service';
+import { CategoryService } from './services/category/category.service';
 import { DeviceService } from './services/device/device.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, DeviceController],
-  providers: [AppService, DeviceService, DeviceRepository, PrismaService],
+  controllers: [AppController, DeviceController, CategoryController],
+  providers: [
+    AppService,
+    DeviceService,
+    DeviceRepository,
+    PrismaService,
+    CategoryService,
+    CategoryRepository,
+  ],
 })
 export class AppModule {}
---
+++ src/repositories/category/category.repository.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { CategoryRepository } from './category.repository';
+
+describe('CategoryService', () => {
+  let repository: CategoryRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CategoryRepository],
+    }).compile();
+
+    repository = module.get<CategoryRepository>(CategoryRepository);
+  });
+
+  it('should be defined', () => {
+    expect(repository).toBeDefined();
+  });
+});
