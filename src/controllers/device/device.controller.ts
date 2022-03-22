dca862dd7ac5c838fe789653f4a9058298b70cf4
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-21 22:38:55 -0300

    modified response to include status and errors

--- src/controllers/device/device.controller.ts
+++ src/controllers/device/device.controller.ts
@@ -2,19 +2,30 @@ import { Body, Controller, Delete, Get, HttpStatus, NotFoundException, Param, Pa
 import { Device } from 'src/models/device';
 import { DeviceService } from 'src/services/device/device.service';
 
+import { HttpResponse } from '../http-response';
+
 @Controller('device')
 export class DeviceController {
   constructor(private readonly deviceService: DeviceService) {}
 
   @Get()
-  findDevices(): Promise<(Device | null)[]> {
-    return this.deviceService.findDevices();
+  async findDevices(): Promise<HttpResponse<(Device | null)[]>> {
+    const devices = await this.deviceService.findDevices();
+
+    if (!devices || !devices.length) {
+      throw new NotFoundException({
+        statusCode: HttpStatus.NOT_FOUND,
+        error: `Devices not found`,
+      });
+    }
+
+    return Promise.resolve(new HttpResponse<(Device | null)[]>(devices));
   }
 
   @Get(':id')
   async findDeviceById(
     @Param('id', ParseIntPipe) id: number,
-  ): Promise<Device | null> {
+  ): Promise<HttpResponse<Device>> {
     const device = await this.deviceService.findDeviceById(id);
 
     if (!device) {
@@ -24,16 +35,30 @@ export class DeviceController {
       });
     }
 
-    return device;
+    return Promise.resolve(new HttpResponse<Device>(device));
   }
 
   @Post()
-  createDevice(@Body() device: Device): Promise<Device | null> {
-    return this.deviceService.createNewDevice(device);
+  async createDevice(
+    @Body() device: Device,
+  ): Promise<HttpResponse<Device | null>> {
+    const newDevice = await this.deviceService.createNewDevice(device);
+    return Promise.resolve(new HttpResponse<Device | null>(newDevice));
   }
 
   @Delete(':id')
-  deleteDevice(@Param('id', ParseIntPipe) id: number): Promise<Device | null> {
-    return this.deviceService.deleteDevice(id);
+  async deleteDevice(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<HttpResponse<Device | null>> {
+    const device = await this.deviceService.deleteDevice(id);
+
+    if (!device) {
+      throw new NotFoundException({
+        statusCode: HttpStatus.NOT_FOUND,
+        error: `Could not delete device with id ${id}`,
+      });
+    }
+
+    return Promise.resolve(new HttpResponse<Device | null>(device));
   }
 }
