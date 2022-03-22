f6f85926109a5b5dae5eee9f7b09024d6423b7d2
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-22 09:01:40 -0300

    implement find all in service and repository

--- src/controllers/category/category.controller.ts
+++ src/controllers/category/category.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Category } from 'src/models/category';
+import { CategoryService } from 'src/services/category/category.service';
 
 @Controller('categories')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  findAllCategories(): Promise<(Category | null)[]> {
+    return this.categoryService.findAllCategories();
+  }
+}
--- src/services/category/category.service.ts
+++ src/services/category/category.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Category } from 'src/models/category';
+import { CategoryRepository } from 'src/repositories/category/category.repository';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private readonly categoryRepository: CategoryRepository) {}
+
+  findAllCategories(): Promise<(Category | null)[]> {
+    return this.categoryRepository.findAll();
+  }
+}
