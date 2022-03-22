260894cde1b47464e07fc653d9ce8e4077602051
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-22 09:02:49 -0300

    fix tests for categories

--- src/controllers/category/category.controller.spec.ts
+++ src/controllers/category/category.controller.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CategoryRepository } from 'src/repositories/category/category.repository';
+import { PrismaService } from 'src/repositories/prisma/prisma.service';
+import { CategoryService } from 'src/services/category/category.service';
+
 import { CategoryController } from './category.controller';
 
 describe('CategoryController', () => {
@@ -7,6 +11,7 @@ describe('CategoryController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CategoryController],
+      providers: [CategoryService, CategoryRepository, PrismaService],
     }).compile();
 
     controller = module.get<CategoryController>(CategoryController);
--- src/services/category/category.service.spec.ts
+++ src/services/category/category.service.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CategoryRepository } from 'src/repositories/category/category.repository';
+import { PrismaService } from 'src/repositories/prisma/prisma.service';
+
 import { CategoryService } from './category.service';
 
 describe('CategoryService', () => {
@@ -6,7 +9,7 @@ describe('CategoryService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+      providers: [CategoryRepository, CategoryService, PrismaService],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
