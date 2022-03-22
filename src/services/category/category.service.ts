caa46ecb6fabd7846fa0ec57daf6d8def1736eb9
Jonathas Nascimento <nasc.jonathas@gmail.com>
2022-03-22 13:34:39 -0300

    added findCategoryById, createNewCategory and deleteCategory

--- src/services/category/category.service.ts
+++ src/services/category/category.service.ts
@@ -9,4 +9,16 @@ export class CategoryService {
   findAllCategories(): Promise<(Category | null)[]> {
     return this.categoryRepository.findAll();
   }
+
+  findCategoryById(id: number): Promise<Category | null> {
+    return this.categoryRepository.findById(id);
+  }
+
+  createNewCategory(category: Category): Promise<Category | null> {
+    return this.categoryRepository.createNew(category);
+  }
+
+  deleteCategory(id: number): Promise<Category | null> {
+    return this.categoryRepository.delete(id);
+  }
 }
